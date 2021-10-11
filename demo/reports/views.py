c6449325fe8947c2952a9ea244af62cdf2343c10
Tornike Skhulukhia <tornike.skhulukhia@iliauni.edu.ge>
2021-10-11 12:58:49 +0400

    rm breakpoint

--- demo/reports/views.py
+++ demo/reports/views.py
@@ -39,7 +39,6 @@ def handle_data_submission(request):
 
     form = ReportForm(request.POST, request.FILES)
 
-    breakpoint()
     if not form.is_valid():
         return JsonResponse({"errors": [", ".join(i) for i in form.errors.values()]})
 
